e7d891bbce11a44bbf6f25f915f3d64457950414
SlangTang <93gaget@gmail.com>
2025-02-14 11:51:06 -0600

    fixed a bug with the email field

---
+++ server/dist/routes/signUp.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+// import bcrypt from 'bcrypt';
+export const signUp = async (req, res) => {
+    const { username, password } = req.body;
+    //   const user = await User.findOne({
+    //     where: { username },
+    //   });
+    //   if (!user) {
+    //     return res.status(401).json({ message: 'Authentication failed' });
+    //   }
+    //   const passwordIsValid = await bcrypt.compare(password, user.password);
+    //   if (!passwordIsValid) {
+    //     return res.status(401).json({ message: 'Authentication failed' });
+    //   }
+    try {
+        const newUser = await User.create({ username, password });
+        res.status(201).json(newUser);
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '7d' });
+    return res.json({ token });
+};
+const router = Router();
+// POST /login - Login a user
+router.post('/signUp', signUp);
+export default router;
