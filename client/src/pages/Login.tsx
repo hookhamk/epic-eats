9d68149fbb7cc9aecf2a4e28c871bc7475cf91c0
SlangTang <93gaget@gmail.com>
2025-02-16 10:18:55 -0600

    finished login and sign up page and forms

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,11 @@
 import { useState, type FormEvent, type ChangeEvent } from 'react';
-
+import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 import { login } from '../api/authAPI';
 import type { UserLogin } from '../interfaces/UserLogin';
+
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Flex } from 'antd';
 
 const Login = () => {
   const [loginData, setLoginData] = useState<UserLogin>({
@@ -32,34 +35,48 @@ const Login = () => {
 
   return (
     <div className='form-container'>
-      <form className='form login-form' onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <div className='form-group'>
-          <label>Username</label>
-          <input
-            className='form-input'
-            type='text'
-            name='username'
+      <Form className='form login-form' 
+        onSubmit={handleSubmit}
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+      >
+        <Form.Item className='form-group'
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input
+            prefix={<UserOutlined />} 
+            placeholder="Username"
             value={loginData.username || ''}
             onChange={handleChange}
           />
-        </div>
-        <div className='form-group'>
-          <label>Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password'
+        </Form.Item>
+        <Form.Item className='form-group'
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input
+            prefix={<LockOutlined />} type="password" placeholder="Password" 
             value={loginData.password || ''}
             onChange={handleChange}
           />
-        </div>
-        <div className='form-group'>
-          <button className='btn btn-primary' type='submit'>
-            Login
-          </button>
-        </div>
-      </form>
+        </Form.Item>
+        <Form.Item className='form-group'>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+              <a href="">Forgot password</a>
+            </Flex>
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+            or <Link to='/signup'>Register Now!</Link>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
